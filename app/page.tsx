e1a3f69f3e9b6918e8a8dac87c03fc1138b2837d
Gautham Vanjre <vanjregautham@gmail.com>
2024-06-07 16:40:19 +0530

    feat: removed boilerplate code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import prisma from "@/prisma/prisma";
 
 export default async function Home() {
-  const users = await prisma.user.findMany({});
-
-  return <h2>{JSON.stringify(users)}</h2>;
+  return <h2>Hello World</h2>;
 }
